243ef3c99081a11bb4a2f27f8ecbd89beebf80e7
Mahad-Ghauri <mahadghauri222@gmail.com>
2025-05-22 20:07:24 +0500

    fix: update ndkVersion in build.gradle.kts and replace placeholder in main.dart with LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recycle_application/Views/Authentication/login_scrreen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,12 +15,6 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: LoginPage());
   }
 }
